5f977bf7485f5abdb0b784b6f9d631d49c8ea5cf
Clarke Harris <clarkeharris@gmail.com>
2014-06-04 02:07:39 -0400

    Began constructing MVC to handle new tasks to posted

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,56 @@
-console.log('\'Allo \'Allo!');
+
+Checklist = Backbone.Model.extend({
+ 
+  initialize: function() {
+    this.on('change', function(){
+    })
+  },
+ 
+  idAttribute: '_id'
+});
+// Collection
+ChecklistCollection = Backbone.Collection.extend({
+ 
+  model: Checklist,
+ 
+  url: 'http://tiny-pizza-server.herokuapp.com/collections/ch-todo',
+})
+
+// AddCheckList View
+
+AddChecklistView = Backbone.View.extend({
+	
+	template: _.template($('.checklist-item').text()),	
+
+	events: {
+		'click. .add-button'  : 'addTask'
+	
+	},
+
+	  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+    
+    $('submit-task').append(this.el);
+    this.render();
+    },
+
+    render: function(){
+    	var renderedTemplate = this.template(this.model.attributes)
+    },
+
+    addTask: function(){
+	    var nameVal = this.$el.find('.task input').val();
+	    this.model.set('task', nameVal);
+	    this.model.save()
+  	},
+
+})
+ 
+})
+ var items = new ChecklistCollection();
+
+ items.fetch().done(function(){
+ 	items.each(function(checklist){
+ 		new ChecklistView({model: checklist});
+ 	})
+ });
