4dabae0db071013cd54124af7d86289dde41e0dc
Clarke Harris <clarkeharris@gmail.com>
2014-06-04 22:36:10 -0400

    Adds, edits, saves, deletes, tasks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,4 @@
-
-Checklist = Backbone.Model.extend({
+Tasks = Backbone.Model.extend({
  
   initialize: function() {
     this.on('change', function(){
@@ -7,50 +6,88 @@ Checklist = Backbone.Model.extend({
   },
  
   idAttribute: '_id'
+
 });
-// Collection
-ChecklistCollection = Backbone.Collection.extend({
  
-  model: Checklist,
+TasksCollection = Backbone.Collection.extend({
  
+  model: Tasks,
   url: 'http://tiny-pizza-server.herokuapp.com/collections/ch-todo',
-})
-
-// AddCheckList View
-
-AddChecklistView = Backbone.View.extend({
-	
-	template: _.template($('.checklist-item').text()),	
-
-	events: {
-		'click. .add-button'  : 'addTask'
-	
-	},
-
-	  initialize: function() {
-    this.listenTo(this.model, 'change', this.render);
-    
-    $('submit-task').append(this.el);
-    this.render();
-    },
-
-    render: function(){
-    	var renderedTemplate = this.template(this.model.attributes)
-    },
-
-    addTask: function(){
-	    var nameVal = this.$el.find('.task input').val();
-	    this.model.set('task', nameVal);
-	    this.model.save()
-  	},
 
 })
  
-})
- var items = new ChecklistCollection();
+TasksView = Backbone.View.extend({
+ 
+  template: _.template($('.user-list-item').text()),
+  editTemplate: _.template($('.user-list-edit-item').text()),
+ 
+  events: {
+    'click .edit-button'    : 'editTask',
+    'click .save-button'    : 'saveChanges',
+    'click .delete-button'  : 'destroy',
+    'keydown input'         : 'showChanges'
+  },
+ 
+  initialize: function(){
+ 
+    this.listenTo(this.model, 'change', this.render);
+ 
+ 
+    $('.center-column').prepend(this.el);
+    this.render();
+  },
+ 
+  render: function(){
+    var renderedTemplate = this.template(this.model.attributes)
+    this.$el.html(renderedTemplate);
+  },
+ 
+  editTask: function(){
+    var renderedTemplate = this.editTemplate(this.model.attributes)
+    this.$el.html(renderedTemplate);
+  },
+ 
+  saveChanges: function(){
+    var nameVal = this.$el.find('.task input').val();
+    this.model.set('task', nameVal);
+    this.model.save()
+  },
+ 
+  destroy: function(){
+    this.model.destroy();
+    this.remove();
+  },
+ 
+  showChanges: function(){
+    if (this.model.get('task') !== this.$el.find('.task input').val()){
+      this.$el.find('.task input').addClass('changed')
+    } else {
+      this.$el.find('.task input').removeClass('changed')
+    }
+  }
 
- items.fetch().done(function(){
- 	items.each(function(checklist){
- 		new ChecklistView({model: checklist});
- 	})
- });
+})
+ 
+$(function(){
+  $('.add-new').click(function(){
+    var inputVal = $('.add-new-input').val()
+    var newUserInstance = tasks.add({task: inputVal})
+ 
+    newUserInstance.save()
+ 
+  })
+})
+ 
+AddTasksView = Backbone.View.extend({
+ 
+  initialize: function(){
+    this.listenTo(tasks, 'add', function(user){
+      new TasksView({model: user})
+    })
+  }
+ 
+});
+ var tasks = new TasksCollection();
+var app = new AddTasksView();
+tasks.fetch();
+// create instances
